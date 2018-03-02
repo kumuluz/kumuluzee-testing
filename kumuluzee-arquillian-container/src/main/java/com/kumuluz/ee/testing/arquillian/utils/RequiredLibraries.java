cd17356569f7dbeb69b98460f62c812e863c6525
Urban Malc <urban.malc@cloud.si>
2018-03-02 12:47:50 +0100

    Added headers

--- kumuluzee-arquillian-container/src/main/java/com/kumuluz/ee/testing/arquillian/utils/RequiredLibraries.java
+++ kumuluzee-arquillian-container/src/main/java/com/kumuluz/ee/testing/arquillian/utils/RequiredLibraries.java
@@ -1,3 +1,23 @@
+/*
+ *  Copyright (c) 2014-2017 Kumuluz and/or its affiliates
+ *  and other contributors as indicated by the @author tags and
+ *  the contributor list.
+ *
+ *  Licensed under the MIT License (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  https://opensource.org/licenses/MIT
+ *
+ *  The software is provided "AS IS", WITHOUT WARRANTY OF ANY KIND, express or
+ *  implied, including but not limited to the warranties of merchantability,
+ *  fitness for a particular purpose and noninfringement. in no event shall the
+ *  authors or copyright holders be liable for any claim, damages or other
+ *  liability, whether in an action of contract, tort or otherwise, arising from,
+ *  out of or in connection with the software or the use or other dealings in the
+ *  software. See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.kumuluz.ee.testing.arquillian.utils;
 
 import com.kumuluz.ee.testing.arquillian.KumuluzEEContainerConfig;
@@ -14,6 +34,12 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Includes methods used for collecting libraries, required for each deployment.
+ *
+ * @author Urban Malc
+ * @since 1.0.0
+ */
 public class RequiredLibraries {
 
     private static final String[] LIBS_DEFAULT = {
@@ -34,6 +60,12 @@ public class RequiredLibraries {
             "com.kumuluz.ee:kumuluzee-microProfile-1.2:",
     };
 
+    /**
+     * Returns libraries, required for each deployment (specified either using {@link MavenDependencyAppender} or
+     * includeRequiredLibraries configuration property).
+     *
+     * @return Array of required libraries.
+     */
     public static File[] getRequiredLibraries() {
         List<String> libraries = MavenDependencyAppender.getDeclaredLibraries();
 
@@ -65,6 +97,12 @@ public class RequiredLibraries {
 
     }
 
+    /**
+     * Returns a list of required dependencies, specified with includeRequiredLibraries configuration property.
+     *
+     * @param includeRequiredLibraries includeRequiredLibraries configuration property
+     * @return A list of dependencies.
+     */
     private static List<String> getIncludedLibraries(String includeRequiredLibraries) {
         switch (includeRequiredLibraries) {
             case KumuluzEEContainerConfig.INCLUDE_LIBS_FALSE:
